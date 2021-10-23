48f9ef848a2fb17aff53b92d18d0b2a2acb80327
estevaoreis25 <estevaoreis1587@gmail.com>
2021-10-23 16:46:00 -0300

    Created a authentication with github and start to work with prisma

--- node_heat/src/services/AuthenticateUserService.ts
+++ node_heat/src/services/AuthenticateUserService.ts
@@ -1,4 +1,6 @@
 import axios from "axios"
+import "dotenv/config"
+import prismaClient from "../prisma"
 /**
  * Receber code(string)
  * REcuperar o acess_token no github
@@ -8,10 +10,22 @@ import axios from "axios"
  * Retorn o token com as infos do user
  */
 
+interface IAccessTokenResponse{
+  access_token: string
+}
+
+interface IUserResponse{
+  avatar_url: string,
+  login: string,
+  id: number,
+  name: string
+}
+
 class AuthenticateUserService {
   async execute(code: string){
-    const url = "https://github.com/login/oauth/acess_token";
-    const response = await axios.post(url, null, {
+    const url = "https://github.com/login/oauth/access_token";
+    const {data: accessTokenResponse} = await axios.post<IAccessTokenResponse>(url, null, 
+      {
       params: {
         client_id: process.env.GITHUB_CLIENT_ID,
         client_secret: process.env.GITHUB_CLIENT_SECRET,
@@ -20,8 +34,32 @@ class AuthenticateUserService {
       headers:{
         "Accept": "application/json"
       }
+    });
+    
+    const response = await axios.get<IUserResponse>("http://api.github.com/user", {
+      headers: {
+        authorization: `Bearer ${accessTokenResponse.access_token}`
+      }
+    });
+    
+    const {login, id, avatar_url, name} = response.data;
+
+    const user = await prismaClient.user.findFirst({
+      where:{
+        github_id: id
+      }
     })
 
+    if(!user){
+      await prismaClient.user.create({
+        data:{
+          github_id:id,
+          login,
+          avatar_url,
+          name
+        }
+      })
+    }
     return response.data;
   }
 }

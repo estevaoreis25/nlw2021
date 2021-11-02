14e6eeef33a607defe1ca4e43e55b247d0e4d687
estevaoreis25 <estevaoreis1587@gmail.com>
2021-11-02 17:58:11 -0300

    Created Contoller and Service to profile user

--- node_heat/src/routes.ts
+++ node_heat/src/routes.ts
@@ -2,7 +2,9 @@ import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateMessageController } from "./controllers/CreateMessageController";
 import { GetLast3MessagesController } from "./controllers/GetLast3MessagesController";
+import { ProfileUserController } from "./controllers/ProfileUserController";
 import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
+import { ProfileUserService } from "./services/ProfileUserService";
 
 const router = Router();
 
@@ -13,5 +15,6 @@ router.post("/messages",
             new CreateMessageController().handle);
 
 router.get("/messages/last3", new GetLast3MessagesController().handle)
+router.get("/profile",ensureAuthenticated, new ProfileUserController().handle)
 
 export {router};
